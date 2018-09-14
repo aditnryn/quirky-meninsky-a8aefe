b0c0d2a31df337effbbd8437f6b17cdc00dc8462
Aditya Narayan <mail@narayanaditya.in>
2018-09-14 20:24:17 +0530

    Implemented Netlify auth

---
+++ assets/js/netlify-variables.js
@@ -0,0 +1,6 @@
+var AWS_REGION = 'us-east-2';
+var USER_POOL_ID = 'us-east-2_GyAPyyVFi';
+var USER_POOL_CLIENT_ID = '161ova2h86c77f1229sjfhgqu2';
+var IDENTITY_POOL_ID = 'us-east-2:46ed1ed8-8c41-4e9b-b16f-38c3a075b8d9';
+var DYNAMODB_TABLE_NAME = 'tier';
+var YOUR_USER_POOL_ID_IDP = 'cognito-idp.us-east-2.amazonaws.com/us-east-2_GyAPyyVFi';
